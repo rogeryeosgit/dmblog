b812108d9f6d591f2effcd3a9dc22d536d8add52
RogerY <roger@geekshacking.com>
2023-01-11 18:16:15 +0800

    add meta descriptions and wordings

--- src/components/BlogFooter.vue
+++ src/components/BlogFooter.vue
@@ -2,7 +2,7 @@
   <transition appear name="v--mask">
     <footer v-once class="blog__footer">
       <div class="signature cosmicjs">
-        Proudly powered by <a href="https://cosmicjs.com" target="_blank">Cosmic JS</a>
+        Inspired gleanings fueled by domains
       </div>
     </footer>
   </transition>
