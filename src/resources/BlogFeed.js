931f665fc03a1158baf6424663381136ead3ec37
RogerY <roger@geekshacking.com>
2023-08-14 15:45:06 +0800

    fixing getting only posts

--- src/resources/BlogFeed.js
+++ src/resources/BlogFeed.js
@@ -3,19 +3,21 @@ import _map from 'lodash.map'
 export default {
   feed() {
     return {
-      path: '/objects?type=posts',
+      path: '/objects',
       resolve: (response, mappers) => {
         let _posts = _map(response.objects, function (i) {
-          let entry = Object.entries(i)
-          console.log(entry);
-          let temp = {
-            title: i.title,
-            image: i.metadata.image.imgix_url,
-            published: i.created_at,
-            author: i.metadata.author.title,
-            id: i.slug
-          };
-          return temp;
+          if (i.type == 'posts') {
+            let entry = Object.entries(i)
+            console.log(entry);
+            let temp = {
+              title: i.title,
+              image: i.metadata.image.imgix_url,
+              published: i.created_at,
+              author: i.metadata.author.title,
+              id: i.slug
+            };
+            return temp;
+          }
         })
         return mappers.pipe(_posts)
       }
