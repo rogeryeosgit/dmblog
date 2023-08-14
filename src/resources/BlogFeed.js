f8033bafc0c5085d7874c67a7867d0483e3c532f
RogerY <roger@geekshacking.com>
2023-08-14 15:20:13 +0800

    figuring out what is returned

--- src/resources/BlogFeed.js
+++ src/resources/BlogFeed.js
@@ -6,9 +6,10 @@ export default {
       path: '/objects',
       resolve: (response, mappers) => {
         let _posts = _map(response.objects, function(i) {
+          console.log("This is i:" + i);
           let temp = {
             title: i.title,
-            image: i.metadata.image.url,
+            image: i.metadata.image.imgix_url,
             published: i.created_at,
             author: i.metadata.author.title,
             id: i.slug
